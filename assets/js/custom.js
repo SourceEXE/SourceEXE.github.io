f4811de37ecf06ab2bd5dbc2a678f0743d3021cf
SourceEXE <cristianruiz022@gmail.com>
2025-03-13 16:57:36 +0100

    V2 W/Carrusel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -88,19 +88,19 @@
 		dots: false,
 		nav: true,
 		autoplay: true,
-		margin:30,
+		margin:10,
 		responsive:{
 			  0:{
-				  items:1
-			  },
-			  600:{
 				  items:2
 			  },
-			  1200:{
+			  600:{
 				  items:3
 			  },
+			  1200:{
+				  items:4
+			  },
 			  1800:{
-				items:3
+				items:5
 			}
 		}
 	})
